5f9fc585133b2946b3dfb107697740e67263d56a
Samrat798 <majumdersamrat26@gmail.com>
2024-08-03 12:10:37 +0530

    aster

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
-const Stats = () => {
+const Stats = ({ items }) => {
+  if (!items.length)
+    return (
+      <footer className="stats">
+        Start adding some items to your packing list 🚀
+      </footer>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? 'You got everything! Ready to go ✈️'
+          : `💼 You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 };
